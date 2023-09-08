7a23dff7a11541f7c2332aeb2fc92fc1f0cdeca2
Clément Murzeau <clement@studiorelief.fr>
2023-09-08 16:53:17 +0200

    branch-1.0.5
    opti gsap anim

--- src/utils/gsap.ts
+++ src/utils/gsap.ts
@@ -15,12 +15,12 @@ function stepAnim() {
   gsap.to('.comment_number-w', {
     opacity: 1,
     y: 0,
-    stagger: 0.2, // Effet de "stagger"
+    stagger: 0.5, // Effet de "stagger"
     scrollTrigger: {
-      markers: false,
-      trigger: '.comment_component', // Déclencheur de l'animation
-      start: 'top 55%', // Début de l'animation lorsque le haut du déclencheur atteint 75% du viewport
-      end: 'bottom 80%', // Fin de l'animation lorsque le bas du déclencheur atteint 25% du viewport
+      markers: true,
+      trigger: '.section_comment', // Déclencheur de l'animation
+      start: 'top 60%', // Début de l'animation lorsque le haut du déclencheur atteint 75% du viewport
+      end: 'bottom 85%', // Fin de l'animation lorsque le bas du déclencheur atteint 25% du viewport
       scrub: true, // Lisse l'animation
     },
   });
